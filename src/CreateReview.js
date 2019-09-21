22810952d2e8f529b5aa827b36b74d17dbc75e5e
Jordan Boudreau <jordboudreau@gmail.com>
2019-09-20 22:01:09 -0700

    add success message

--- src/CreateReview.js
+++ src/CreateReview.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 
 export default class CreateReview extends Component {
     state = {
-        body: ''
+        body: '',
+        message: ''
     }
     constructor(props) {
         super(props)
@@ -14,6 +15,7 @@ export default class CreateReview extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault()
+        this.setState({ body: '', message: 'Thanks for submitting!' })
         this.props.submit(this.props.id, this.state.body)
     }
     render() {
@@ -21,10 +23,11 @@ export default class CreateReview extends Component {
             <div className="container">
                 <div className="row">
                     <div className="col-12">
+                        <p style={{ color: 'green' }}>{this.state.message}</p>
                         <form className="d-flex justify-content-center flex-column align-items-center">
                             <label htmlFor="" className='d-block h3'>Create Review</label>
                             <div className="d-flex justify-content-center">
-                                <textarea className="d-block mb-4 p-3" name="body" id="" onChange={this.handleChange}></textarea>
+                                <textarea className="d-block mb-4 p-3" name="body" id="" onChange={this.handleChange} value={this.state.body}></textarea>
                             </div>
 
                             <button onClick={this.handleSubmit} className='btn btn-primary btn-lg'>SUBMIT REVIEW</button>
