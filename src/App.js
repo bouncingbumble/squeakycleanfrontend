478a6d477d113654816f955cb1c1507d575c01b4
Jordan Boudreau <jordboudreau@gmail.com>
2019-09-21 11:06:29 -0700

    ui improvements

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { apiCall } from './api';
 import CreateReview from "./CreateReview"
+import Service from './Service'
+import './App.css'
 
 class App extends Component {
     state = {
@@ -16,7 +18,7 @@ class App extends Component {
         this.setState({ services: services })
     }
 
-    viewLawyer(id) {
+    viewLawyer = (id) => {
 
         // get the lawyer object saved into localStorage
         let lawyer = JSON.parse(window.localStorage.getItem(id))
@@ -47,6 +49,7 @@ class App extends Component {
         this.setState({
             lawyer: service
         })
+        this.getServices()
     }
 
     downvote = async serviceId => {
@@ -55,63 +58,23 @@ class App extends Component {
         this.setState({
             lawyer: service
         })
+        this.getServices()
     }
 
-
-
     render() {
-
-        let table = (
-            <div className="container" style={{ marginTop: "40px" }} id="table-container">
-                <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Name</th>
-                            <th scope="col">Address</th>
-                            <th scope="col">Phone No</th>
-                            <th scope="col">Upvotes</th>
-                            <th scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.state.services ? this.state.services.map(s => {
-                            {
-                                window.localStorage.setItem(s._id, JSON.stringify(s))
-
-                                return (
-                                    <tr className="table-primary">
-                                        <th scope="row"> {s.name} </th>
-                                        <td> {s.address ? s.address.address1 : ""}</td>
-                                        <td> {s.phone} </td>
-                                        <td> {s.votes} </td>
-                                        <td>
-                                            <button href={"/" + s._id} type="button" className="btn btn-secondary" onClick={() => this.viewLawyer(s._id)}> View Reviews </button>
-                                        </td>
-                                    </tr>
-                                )
-                            }
-                        }) : null}
-
-                    </tbody>
-                </table>
-            </div>
-        )
-
+        let services = this.state.services ? this.state.services.map(service => {
+            return <Service service={service} viewLawyer={this.viewLawyer} upvote={this.upvote} downvote={this.downvote}></Service>
+        }) : null
 
         let lawyer = (
-            <div className="animated slideInUp" id="show-one-container" style={{ marginTop: "40px" }}>
+            <div className="animated slideInUp shadow" id="show-one-container" style={{ marginTop: "40px" }} style={{ border: 'none', borderRadius: '5px' }}>
                 <div className="container">
-                    <div className="card" style={{ width: "400px", margin: '0 auto' }}>
-                        <img className="card-img-top" src="http://pappalardolaw.com/wp-content/uploads/2017/02/lawyer-placeholder-female.jpg" alt="Card image" />
+                    <div className="">
                         <div className="card-body">
                             <h4 className="card-title"> {this.state.lawyer ? this.state.lawyer.name : ""}</h4>
                             <p className="d-flex justify-content-center card-text" style={{ margin: "0 auto" }}>
 
-                                <li className="list-group-item">
-                                    <i className="fa fa-arrow-up" onClick={() => { this.upvote(this.state.lawyer._id) }}></i>
-                                    <span className="label label-primary"> {this.state.lawyer ? this.state.lawyer.votes : ""} </span>
-                                    <i className="fa fa-arrow-down" onClick={() => { this.downvote(this.state.lawyer._id) }}></i>
-                                </li>
+
                             </p>
                             <CreateReview id={this.state.lawyer ? this.state.lawyer._id : ""} submit={this.createReview} />
                         </div>
@@ -120,28 +83,31 @@ class App extends Component {
             </div>
         )
 
-        let tableContent = this.state.showLawyer ? null : table
         let lawyerContent = this.state.showLawyer ? lawyer : null
 
         return (
             <div className="App">
-
-
-                { /* NAV BAR */}
-                <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top" style={{ borderStyle: 'solid' }}>
-                    <img src="https://www.pinclipart.com/picdir/big/422-4223386_duck-clipart-bathroom-rubber-duck-with-sunglasses-png.png" alt="" style={{ maxWidth: '75px', maxHeight: '75px' }} />
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary sticky-top shadow" style={{ backgroundColor: '#226CE0' }}>
+                    <img src="https://www.pinclipart.com/picdir/big/422-4223386_duck-clipart-bathroom-rubber-duck-with-sunglasses-png.png" alt="" style={{ maxWidth: '75px', maxHeight: '75px' }} className="hover" />
                     <a className="pl-3 navbar-brand" href="#"> SQUEAKY CLEAN - clean your record</a>
                 </nav>
 
-                { /**/}
-                {tableContent}
-
-
-                {/*container for the reviews*/}
-                {lawyerContent}
-
-
-
+                <div className="container mx-0">
+                    <div className="row">
+                        <div className="col-8">
+                            <div className="row justify-content-center">
+                                <div className="col-8">
+                                    <div className="row justify-content-center">
+                                        {services}
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-4">
+                            {lawyerContent}
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
